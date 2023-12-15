c02abd6d82734f9bd0dcc8b4b3913d4e2961070d
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2023-12-16 00:51:54 +0500

    just upload code. INWORK

--- source/cogs/audioplayer.py
+++ source/cogs/audioplayer.py
@@ -1,19 +1,26 @@
 import disnake
-from source.packages.single_video_downloader import download_audio
+from packages.single_video_downloader import download_audio
 from disnake.ext import commands
+
+
 
 class AudioPlayer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name="play",
-                            description="Download video from youtube and play it in a voice channel")
-    async def download(self, ctx, video_url: str):
+    @commands.slash_command(name="griffy",
+                            description="Join bot and play music")
+    async def download(self, ctx):
+        input_link = disnake.ui.Modal.add_text_input(ctx,
+                                                     label="Youtube URL",
+                                                     custom_id="url",
+                                                     style=disnake.TextInputStyle.single_line)
         await ctx.send(f"Downloading video from {video_url}...", ephemeral=True)
         path = await download_audio(video_url)
         channel = ctx.author.voice.channel
         voice_client = await channel.connect()
         voice_client.play(disnake.FFmpegOpusAudio(path))
+
 
 
 
--- source/packages/single_video_downloader.py
+++ source/packages/single_video_downloader.py
@@ -6,11 +6,11 @@ from icecream import ic
 async def download_audio(video_url: str, is_debug=False) -> str|bool:
     try:
         yt = YouTube(video_url, use_oauth=True, allow_oauth_cache=True)
-        cur_date = datetime.today()
-        date = {"year": cur_date.year, "month": cur_date.month, "day": cur_date.day}
     except VideoUnavailable:
         ic(f"Video {video_url} is not available")
     else:
+        cur_date = datetime.today()
+        date = {"year": cur_date.year, "month": cur_date.month, "day": cur_date.day}
         streams = yt.streams.filter(only_audio=True).first()
         if is_debug:
             ic(streams)
